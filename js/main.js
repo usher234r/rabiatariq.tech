1653af4454b512e1ad93d25f2d66c0900013ac39
Hamza Tariq <hamzatrq116@gmail.com>
2023-01-12 00:49:03 +0500

    Updating code to lazy load the video

--- js/main.js
+++ js/main.js
@@ -1,12 +1,46 @@
+// this script lazy loads videos
+document.addEventListener("DOMContentLoaded", function () {
+  var lazyVideos = [].slice.call(document.querySelectorAll("video.lazy"));
+
+  if ("IntersectionObserver" in window) {
+    var lazyVideoObserver = new IntersectionObserver(function (
+      entries,
+      observer
+    ) {
+      entries.forEach(function (video) {
+        if (video.isIntersecting) {
+          for (var source in video.target.children) {
+            var videoSource = video.target.children[source];
+            if (
+              typeof videoSource.tagName === "string" &&
+              videoSource.tagName === "SOURCE"
+            ) {
+              videoSource.src = videoSource.dataset.src;
+            }
+          }
+
+          video.target.load();
+          video.target.classList.remove("lazy");
+          lazyVideoObserver.unobserve(video.target);
+        }
+      });
+    });
+
+    lazyVideos.forEach(function (lazyVideo) {
+      lazyVideoObserver.observe(lazyVideo);
+    });
+  }
+});
+
 (function ($) {
   "use strict";
   document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     anchor.addEventListener("click", function (e) {
-      if (`${e.target.className}`.includes('menu')) {
+      if (`${e.target.className}`.includes("menu")) {
         e.preventDefault();
         document.querySelector(this.getAttribute("href")).scrollIntoView({
           behavior: "smooth",
-        }); 
+        });
       }
     });
   });
